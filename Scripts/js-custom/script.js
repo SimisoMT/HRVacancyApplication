f447b237f15eecf254df45521b4c84abbdca39e7
Simiso Mthiyane <51191238+SimisoMT@users.noreply.github.com>
2024-06-19 22:52:07 +0200

    uploaded rest of files

---
+++ Scripts/js-custom/script.js
@@ -0,0 +1,13 @@
+﻿document.getElementById("checkBoxShowBeingTypedPassword").addEventListener('change', function () {
+
+    
+    if (this.checked) {
+        let inputElements = document.querySelectorAll("input")
+        for (let inputElement of inputElements) { if (inputElement["type"] == "password") { inputElement.setAttribute("type", "text") } }
+    } else {
+        let inputElements = document.querySelectorAll("input")
+        for (let inputElement of inputElements) { if (inputElement["class"] == "password") { inputElement.setAttribute("type", "password") } }
+    }
+
+}), { once: false } ;
+
